9aaf8f9f3df3fcbf301ae44c645879c261206dc9
Jessica Roussy <crystalweb@outlook.fr>
2022-05-19 11:18:57 +0200

    Integration des composants Banner et Cart_Importation du css du composant banner

---
+++ src/composants/Cart.js
@@ -0,0 +1,16 @@
+function Cart() {
+  const monsteraPrice = 8
+  const lierrePrice = 10
+  const bouquet_de_fleurPrice = 15
+  return (
+    <div>
+      <h2>Panier</h2>
+        <ul>
+          <li>Monstera : {monsteraPrice}€</li>
+          <li>Lierre : {lierrePrice}€</li>
+          <li>Bouquet de fleur : {bouquet_de_fleurPrice}€</li>
+        </ul>
+        Total : {monsteraPrice + lierrePrice + bouquet_de_fleurPrice}€
+    </div>)
+}
+export default Cart
